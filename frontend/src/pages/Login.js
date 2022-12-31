dda0f04dd3fc6838052c6a76587c6d2e81c9655c
Folawewo Anthony <famwallexie@gmail.com>
2022-12-31 01:11:01 +0100

    implemented cloudinary

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row, Container, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
@@ -11,7 +12,7 @@ function Login() {
           md={7}
           className="d-flex align-items-center justify-content-center flex-direction-column"
         >
-          <Form>
+          <Form style={{ width: '80%', maxWidth: 500 }}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
@@ -24,12 +25,14 @@ function Login() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Login
             </Button>
+            <div className="py-4">
+              <p className="text-center">
+                Don't have an account ? <Link to="/signup">Signup</Link>
+              </p>
+            </div>
           </Form>
         </Col>
       </Row>
