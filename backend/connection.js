43276a216b7a20deee333475ca44afdb17f454ec
Folawewo Anthony <famwallexie@gmail.com>
2023-01-03 20:21:28 +0100

    creating user model

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.set('strictQuery', false);
 mongoose.connect(
   `mongodb+srv://$(process.env.DB_USER):$(process.env.DB_PASS)@cluster0.vhsqijh.mongodb.net/bingeTexts?retryWrites=true&w=majority`,
   () => {
