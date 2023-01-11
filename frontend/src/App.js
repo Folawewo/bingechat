f000fc9bcd0d3e0ddbaa8671d972541a67361373
Folawewo Anthony <famwallexie@gmail.com>
2023-01-11 22:21:17 +0100

    refactoring user model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
@@ -5,8 +6,15 @@ import Chat from './pages/Chat';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { AppContext, socket } from './context/appContext';
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [privateMembersMsg, setPrivateMemberMsg] = useState({});
+  const [newMessages, setNewMessages] = useState({});
   const user = useSelector((state) => state.user);
   return (
     <BrowserRouter>
